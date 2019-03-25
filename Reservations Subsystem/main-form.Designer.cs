1a15bf62121504c385b726c939dd6f72e96923d2
ainz ooal gown <ainzbedo1@gmail.com>
2019-03-25 23:00:41 +0800

    pushy

--- Reservations Subsystem/main-form.Designer.cs
+++ Reservations Subsystem/main-form.Designer.cs
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             this.btn_Exit = new System.Windows.Forms.Button();
-            this.btn_POS = new System.Windows.Forms.Button();
-            this.btn_Inv = new System.Windows.Forms.Button();
-            this.btn_Sched = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.btn_Acc = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.btn_POS = new System.Windows.Forms.Button();
+            this.btn_Inv = new System.Windows.Forms.Button();
+            this.btn_Sched = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -56,54 +56,6 @@
             this.btn_Exit.Text = "EXIT";
             this.btn_Exit.UseVisualStyleBackColor = false;
             this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
-            // 
-            // btn_POS
-            // 
-            this.btn_POS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(127)))), ((int)(((byte)(0)))));
-            this.btn_POS.FlatAppearance.BorderSize = 0;
-            this.btn_POS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_POS.Font = new System.Drawing.Font("Segoe UI Semilight", 19F);
-            this.btn_POS.ForeColor = System.Drawing.Color.White;
-            this.btn_POS.Location = new System.Drawing.Point(310, 350);
-            this.btn_POS.Margin = new System.Windows.Forms.Padding(0);
-            this.btn_POS.Name = "btn_POS";
-            this.btn_POS.Size = new System.Drawing.Size(155, 127);
-            this.btn_POS.TabIndex = 12;
-            this.btn_POS.Text = "POS";
-            this.btn_POS.UseVisualStyleBackColor = false;
-            this.btn_POS.Click += new System.EventHandler(this.btn_POS_Click);
-            // 
-            // btn_Inv
-            // 
-            this.btn_Inv.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(127)))), ((int)(((byte)(0)))));
-            this.btn_Inv.FlatAppearance.BorderSize = 0;
-            this.btn_Inv.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_Inv.Font = new System.Drawing.Font("Segoe UI Semilight", 19F);
-            this.btn_Inv.ForeColor = System.Drawing.Color.White;
-            this.btn_Inv.Location = new System.Drawing.Point(155, 350);
-            this.btn_Inv.Margin = new System.Windows.Forms.Padding(0);
-            this.btn_Inv.Name = "btn_Inv";
-            this.btn_Inv.Size = new System.Drawing.Size(155, 127);
-            this.btn_Inv.TabIndex = 11;
-            this.btn_Inv.Text = "Inventory";
-            this.btn_Inv.UseVisualStyleBackColor = false;
-            this.btn_Inv.Click += new System.EventHandler(this.btn_Inv_Click);
-            // 
-            // btn_Sched
-            // 
-            this.btn_Sched.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(127)))), ((int)(((byte)(0)))));
-            this.btn_Sched.FlatAppearance.BorderSize = 0;
-            this.btn_Sched.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_Sched.Font = new System.Drawing.Font("Segoe UI Semilight", 19F);
-            this.btn_Sched.ForeColor = System.Drawing.Color.White;
-            this.btn_Sched.Location = new System.Drawing.Point(0, 350);
-            this.btn_Sched.Margin = new System.Windows.Forms.Padding(0);
-            this.btn_Sched.Name = "btn_Sched";
-            this.btn_Sched.Size = new System.Drawing.Size(155, 127);
-            this.btn_Sched.TabIndex = 10;
-            this.btn_Sched.Text = "Schedule";
-            this.btn_Sched.UseVisualStyleBackColor = false;
-            this.btn_Sched.Click += new System.EventHandler(this.btn_Sched_Click);
             // 
             // panel1
             // 
@@ -131,6 +83,8 @@
             // btn_Acc
             // 
             this.btn_Acc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(127)))), ((int)(((byte)(0)))));
+            this.btn_Acc.BackgroundImage = global::Reservations_Subsystem.Properties.Resources.round_account_button_with_user_inside;
+            this.btn_Acc.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.btn_Acc.FlatAppearance.BorderSize = 0;
             this.btn_Acc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_Acc.Font = new System.Drawing.Font("Segoe UI Semilight", 19F);
@@ -141,6 +95,7 @@
             this.btn_Acc.Size = new System.Drawing.Size(155, 127);
             this.btn_Acc.TabIndex = 20;
             this.btn_Acc.Text = "Accounts";
+            this.btn_Acc.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btn_Acc.UseVisualStyleBackColor = false;
             // 
             // button1
@@ -182,6 +137,64 @@
             this.button3.Size = new System.Drawing.Size(502, 86);
             this.button3.TabIndex = 19;
             this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btn_POS
+            // 
+            this.btn_POS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(127)))), ((int)(((byte)(0)))));
+            this.btn_POS.BackgroundImage = global::Reservations_Subsystem.Properties.Resources.budget;
+            this.btn_POS.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_POS.FlatAppearance.BorderSize = 0;
+            this.btn_POS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_POS.Font = new System.Drawing.Font("Segoe UI Semilight", 19F);
+            this.btn_POS.ForeColor = System.Drawing.Color.White;
+            this.btn_POS.Location = new System.Drawing.Point(310, 350);
+            this.btn_POS.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_POS.Name = "btn_POS";
+            this.btn_POS.Size = new System.Drawing.Size(155, 127);
+            this.btn_POS.TabIndex = 12;
+            this.btn_POS.Text = "POS";
+            this.btn_POS.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_POS.UseVisualStyleBackColor = false;
+            this.btn_POS.Click += new System.EventHandler(this.btn_POS_Click);
+            // 
+            // btn_Inv
+            // 
+            this.btn_Inv.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(127)))), ((int)(((byte)(0)))));
+            this.btn_Inv.BackgroundImage = global::Reservations_Subsystem.Properties.Resources.chest;
+            this.btn_Inv.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_Inv.FlatAppearance.BorderSize = 0;
+            this.btn_Inv.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Inv.Font = new System.Drawing.Font("Segoe UI Semilight", 19F);
+            this.btn_Inv.ForeColor = System.Drawing.Color.White;
+            this.btn_Inv.Location = new System.Drawing.Point(155, 350);
+            this.btn_Inv.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_Inv.Name = "btn_Inv";
+            this.btn_Inv.Size = new System.Drawing.Size(155, 127);
+            this.btn_Inv.TabIndex = 11;
+            this.btn_Inv.Text = "Inventory";
+            this.btn_Inv.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_Inv.UseVisualStyleBackColor = false;
+            this.btn_Inv.Click += new System.EventHandler(this.btn_Inv_Click);
+            // 
+            // btn_Sched
+            // 
+            this.btn_Sched.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(127)))), ((int)(((byte)(0)))));
+            this.btn_Sched.BackgroundImage = global::Reservations_Subsystem.Properties.Resources.calendar;
+            this.btn_Sched.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_Sched.FlatAppearance.BorderSize = 0;
+            this.btn_Sched.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Sched.Font = new System.Drawing.Font("Segoe UI Semilight", 19F);
+            this.btn_Sched.ForeColor = System.Drawing.Color.White;
+            this.btn_Sched.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_Sched.Location = new System.Drawing.Point(0, 350);
+            this.btn_Sched.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_Sched.Name = "btn_Sched";
+            this.btn_Sched.Size = new System.Drawing.Size(155, 127);
+            this.btn_Sched.TabIndex = 10;
+            this.btn_Sched.Text = "Schedule";
+            this.btn_Sched.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_Sched.UseVisualStyleBackColor = false;
+            this.btn_Sched.Click += new System.EventHandler(this.btn_Sched_Click);
             // 
             // main_form
             // 
