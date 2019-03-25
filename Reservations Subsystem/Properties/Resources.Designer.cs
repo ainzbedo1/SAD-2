b3fdcb4a5f7be9c5d84f6b3bba4eadebf9d5855e
Jureturoh <40516210+Jureturoh@users.noreply.github.com>
2019-03-25 21:56:30 +0800

    Finished Daily Orders

--- Reservations Subsystem/Properties/Resources.Designer.cs
+++ Reservations Subsystem/Properties/Resources.Designer.cs
@@ -133,16 +133,6 @@ namespace Reservations_Subsystem.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap LOGO {
-            get {
-                object obj = ResourceManager.GetObject("LOGO", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap LOGO2 {
             get {
                 object obj = ResourceManager.GetObject("LOGO2", resourceCulture);
