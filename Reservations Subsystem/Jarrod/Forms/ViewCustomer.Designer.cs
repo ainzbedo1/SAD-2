2d27772f9c7ba77d7efa5c056ce8eebebd2a9c4a
ainz ooal gown <ainzbedo1@gmail.com>
2019-03-25 22:16:31 +0800

    tama diay

--- Reservations Subsystem/Jarrod/Forms/ViewCustomer.Designer.cs
+++ Reservations Subsystem/Jarrod/Forms/ViewCustomer.Designer.cs
@@ -128,6 +128,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "x";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // btn_filter
             // 
