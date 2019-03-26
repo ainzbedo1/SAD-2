7b2c8e1520a63af38082e08dd6a65b19d1b08f6b
ainz ooal gown <ainzbedo1@gmail.com>
2019-03-26 11:42:16 +0800

    add room rate

--- Reservations Subsystem/Jarrod/Forms/AddRoomRate.Designer.cs
+++ Reservations Subsystem/Jarrod/Forms/AddRoomRate.Designer.cs
@@ -33,67 +33,141 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // cmbRoomTypes
             // 
+            this.cmbRoomTypes.Font = new System.Drawing.Font("Segoe UI Semilight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbRoomTypes.FormattingEnabled = true;
-            this.cmbRoomTypes.Location = new System.Drawing.Point(35, 78);
+            this.cmbRoomTypes.Location = new System.Drawing.Point(23, 105);
             this.cmbRoomTypes.Name = "cmbRoomTypes";
-            this.cmbRoomTypes.Size = new System.Drawing.Size(121, 21);
+            this.cmbRoomTypes.Size = new System.Drawing.Size(187, 33);
             this.cmbRoomTypes.TabIndex = 0;
             this.cmbRoomTypes.SelectedIndexChanged += new System.EventHandler(this.cmbRoomTypes_SelectedIndexChanged);
             this.cmbRoomTypes.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cmbRoomTypes_KeyPress);
             // 
             // txtRoomRate
             // 
-            this.txtRoomRate.Location = new System.Drawing.Point(180, 79);
+            this.txtRoomRate.Font = new System.Drawing.Font("Segoe UI Semilight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRoomRate.Location = new System.Drawing.Point(234, 105);
             this.txtRoomRate.Name = "txtRoomRate";
-            this.txtRoomRate.Size = new System.Drawing.Size(104, 20);
+            this.txtRoomRate.Size = new System.Drawing.Size(224, 33);
             this.txtRoomRate.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 59);
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semilight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(127)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(22, 77);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.Size = new System.Drawing.Size(116, 25);
             this.label1.TabIndex = 2;
             this.label1.Text = "Room Types:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(177, 59);
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semilight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(127)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(229, 77);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.Size = new System.Drawing.Size(148, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "New Room Rate:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(321, 79);
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(127)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semilight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(27, 171);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(191, 23);
+            this.button1.Size = new System.Drawing.Size(431, 44);
             this.button1.TabIndex = 4;
-            this.button1.Text = "Add RoomRate";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "SAVE";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(127)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.btnClose);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(484, 50);
+            this.panel1.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semilight", 20F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(9, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 37);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Add Room Rate";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold);
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(447, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(37, 50);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "x";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Enabled = false;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(127)))), ((int)(((byte)(0)))));
+            this.button2.FlatAppearance.BorderSize = 4;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(16, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(455, 168);
+            this.button2.TabIndex = 14;
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // AddRoomRate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(551, 127);
+            this.ClientSize = new System.Drawing.Size(484, 236);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtRoomRate);
             this.Controls.Add(this.cmbRoomTypes);
+            this.Controls.Add(this.button2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AddRoomRate";
             this.Text = "AddRoomRate";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AddRoomRate_FormClosing);
             this.Load += new System.EventHandler(this.AddRoomRate_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,5 +180,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button button2;
     }
 }
